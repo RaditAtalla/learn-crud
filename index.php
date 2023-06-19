5bd2a93625be864f3b734ec2d621baa6c6e67201
Raditya Atalla <radityaderachmadie@gmail.com>
2023-06-19 10:22:49 +0700

    format code

--- index.php
+++ index.php
@@ -1,24 +1,26 @@
-<?php 
-    require "functions.php";
+<?php
+require "functions.php";
+$accounts = query("SELECT * FROM akun");
+
+if (isset($_POST['cari'])) {
+    $accounts = cari($_POST['keyword']);
+}
+
+if (isset($_POST['reset'])) {
     $accounts = query("SELECT * FROM akun");
-
-    if (isset($_POST['cari'])) {
-        $accounts = cari($_POST['keyword']);
-    }
-
-    if (isset($_POST['reset'])) {
-        $accounts = query("SELECT * FROM akun");
-    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Account management</title>
 </head>
+
 <body>
     <h1>Account Management</h1>
     <a href="add.php">Add account</a>
@@ -38,20 +40,19 @@
             <th>Password</th>
             <th>Bio</th>
         </tr>
-
-        <?php $i = 1?>
+        <?php $i = 1 ?>
         <?php foreach ($accounts as $account) : ?>
-        <tr>
-            <td><?= $i;?></td>
-            <td>
-                <a href="update.php?id=<?= $account["id"]?>">Change</a> |
-                <a href="delete.php?id=<?= $account["id"]?>" onclick="return confirm('Delete data?');">Delete</a>
-            </td>
-            <td><?= $account["username"]?></td>
-            <td><?= $account["password"]; ?></td>
-            <td><?= $account["bio"]; ?></td>
-        </tr>
-        <?php $i++ ?>
+            <tr>
+                <td><?= $i; ?></td>
+                <td>
+                    <a href="update.php?id=<?= $account["id"] ?>">Change</a> |
+                    <a href="delete.php?id=<?= $account["id"] ?>" onclick="return confirm('Delete data?');">Delete</a>
+                </td>
+                <td><?= $account["username"] ?></td>
+                <td><?= $account["password"]; ?></td>
+                <td><?= $account["bio"]; ?></td>
+            </tr>
+            <?php $i++ ?>
         <?php endforeach; ?>
     </table>
 </body>
